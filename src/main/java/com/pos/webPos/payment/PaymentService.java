df71b158109a7c55038f36d41a4e45eae25fe717
yunaiju <hyejoo04@naver.com>
2025-07-24 15:01:34 +0900

    feat : PaymentServiceTest 작성

--- src/main/java/com/pos/webPos/payment/PaymentService.java
+++ src/main/java/com/pos/webPos/payment/PaymentService.java
@@ -2,50 +2,88 @@ package com.pos.webPos.payment;
 
 import com.pos.webPos.DataNotFoundException;
 import com.pos.webPos.session.PosSession;
+import com.pos.webPos.session.PosSessionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class PaymentService {
     private final PaymentRepository paymentRepository;
+    private final PosSessionRepository posSessionRepository;
 
     public void save(Payment payment) {
         this.paymentRepository.save(payment);
     }
 
-    public void deletePayment(Payment payment) {this.paymentRepository.delete(payment);}
+    public void deletePayment(Payment payment) {
+        if(payment.getId()!=null) {
+            Optional<Payment> validPayment = this.paymentRepository.findById(payment.getId());
+            if(validPayment.isPresent()) {
+                this.paymentRepository.delete(payment);
+            } else {
+                throw new DataNotFoundException("payment not found");
+            }
+        } else {
+            throw new DataNotFoundException("payment not found");
+        }
+    }
 
-    public Payment getPayment(Long id) { return this.paymentRepository.findById(id)
-            .orElseThrow(()-> new DataNotFoundException("payment not found")); }
+    public Payment getPayment(Long id) {
+        if (id != null) {
+            Optional<Payment> validPayment = this.paymentRepository.findById(id);
+            if (validPayment.isPresent()) {
+                return validPayment.get();
+            } else {
+                throw new DataNotFoundException("payment not found");
+            }
+        } else {
+            throw new DataNotFoundException("payment not found");
+        }
+    }
 
     public List<Payment> getPaymentList(PosSession posSession) {
-        return this.paymentRepository.findAllByPosSession(posSession);
+        if(posSession.getId()!=null) {
+            Optional<PosSession> validSession = this.posSessionRepository.findById(posSession.getId());
+            if(validSession.isPresent()) {
+                return this.paymentRepository.findAllByPosSession(posSession);
+            } else {
+                throw new DataNotFoundException("session not found");
+            }
+        } else {
+            throw new DataNotFoundException("session not found");
+        }
     }
 
     public Integer getTotalCashPayments(PosSession posSession) {
-        // '현금' 결제의 totalPrice 합을 구함
-        try {
-            // 데이터베이스 조회 로직
-            return this.paymentRepository.findTotalPriceByPosSessionIdForCashPayments(posSession.getId());
-        } catch (Exception e) {
-            log.error("Error fetching total cash payments", e);
-            return 0;  // 예외 발생 시 기본값 반환
+        if(posSession.getId()!=null) {
+            Optional<PosSession> validSession = this.posSessionRepository.findById(posSession.getId());
+            if(validSession.isPresent()) {
+                return this.paymentRepository.findTotalPriceByPosSessionIdForCashPayments(posSession.getId());
+            } else {
+                throw new DataNotFoundException("session not found");
+            }
+        } else {
+            throw new DataNotFoundException("session not found");
         }
     }
 
     public Integer getTotalCardPayments(PosSession posSession) {
-        // '카드' 결제의 totalPrice 합을 구함
-        try {
-            // 데이터베이스 조회 로직
-            return this.paymentRepository.findTotalPriceByPosSessionIdForCardPayments(posSession.getId());
-        } catch (Exception e) {
-            log.error("Error fetching total cash payments", e);
-            return 0;  // 예외 발생 시 기본값 반환
+        if(posSession.getId()!=null) {
+            Optional<PosSession> validSession = this.posSessionRepository.findById(posSession.getId());
+            if(validSession.isPresent()) {
+                return this.paymentRepository.findTotalPriceByPosSessionIdForCardPayments(posSession.getId());
+            } else {
+                throw new DataNotFoundException("session not found");
+            }
+        } else {
+            throw new DataNotFoundException("session not found");
         }
     }
 }
